be774ae8a982394554fdee47df1fbf8ac61c0d4c
Daniela <daniela.sofrone.sd@gmail.com>
2021-02-15 14:35:26 +0100

    added and deployed with firebase

---
+++ build/precache-manifest.ad5f2e1effa5863e7cfb1399c07902df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59105f781ae88a576aaa7d6102db3a98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c69c2f6170f1aecdcb9",
+    "url": "/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "e2e9ed03ac73c40a3625",
+    "url": "/static/css/main.7bc7f546.chunk.css"
+  },
+  {
+    "revision": "2c69c2f6170f1aecdcb9",
+    "url": "/static/js/2.85dbfdc9.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/static/js/2.85dbfdc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2e9ed03ac73c40a3625",
+    "url": "/static/js/main.a40921a0.chunk.js"
+  },
+  {
+    "revision": "2c27a3b221e7317c545b",
+    "url": "/static/js/runtime-main.a31d5815.js"
+  },
+  {
+    "revision": "57877daab25cc9288b5216e84be7751a",
+    "url": "/static/media/corner.57877daa.svg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/static/media/heart.7746f5e4.svg"
+  },
+  {
+    "revision": "d648020dc8433d8736299c85c8bedb50",
+    "url": "/static/media/leftArow.d648020d.svg"
+  }
+]);
